64928226b6a6adb705fad8a0c7aaeb5a9837d04c
listguy <nitzanjl@gmail.com>
2024-01-26 12:23:19 +0200

    added basic cameras

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -95,4 +95,9 @@ public class Player : MonoBehaviour
         else
             AnimatorComp.SetBool("Walk", false);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
 }
